853801aeef868fa1bf1269ada26e0b5938933d9e
ArunTeltia <teltia.arun18@gmail.com>
2020-01-28 02:42:04 +0530

    get github repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -352,7 +352,7 @@ router.get('/github/:username',(req,res)=>{
             if(error) console.error(error);
 
             if(response.statusCode!==200){
-                res.status(404).json({msg:'No github profile found'});
+                return res.status(404).json({msg:'No github profile found'});
             }
             res.json(JSON.parse(body));
         });
