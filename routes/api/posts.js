5550affe8ef4ce07398a88586f2c21cdaa047d8d
ArunTeltia <teltia.arun18@gmail.com>
2020-01-28 04:01:10 +0530

    get all/one post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -46,4 +46,39 @@ router.post(
   }
 );
 
+//@route get api/posts
+//@desc  get all posts
+//@acess Private
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({date:-1});
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//@route get api/posts/:id
+//@desc  get post by ID
+//@acess Private
+router.get("/:id", auth, async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+ 
+      if(!post){
+          return res.statusMessage(404).json({msg:'Post not found'});
+      }
+      res.json(posts) ; 
+    } catch (err) {
+
+      console.error(err.message);
+      if(err.kind==='ObjectId'){
+          //the id that we are passing dont exist
+        return res.statusMessage(404).json({msg:'Post not found'});
+    }
+      res.status(500).send("Server Error");
+    }
+  });
 module.exports = router;
