857f7cc0c7e4bfdedd5b150a8cc0dcfba4ebc5a4
ArunTeltia <teltia.arun18@gmail.com>
2020-01-27 23:33:37 +0530

    get all profiles & profile by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -97,4 +97,37 @@ router.post('/',[auth,[
             res.status(500).send('Server Error');
         }
 });
+//@route get api/profile
+//@desc get all profiles
+//@acess public
+router.get('/', async (req,res)=>{
+    try {
+        const profiles =await Profile.find().populate('user',['name','avatar']); 
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+
+    }
+}); 
+//@route get api/profile/user/:user_id
+//@desc get profile by user id
+//@acess public
+router.get('/', async (req,res)=>{
+    try {
+        const profiles =await Profile.findOne({user:req.params.user_id}).populate('user',['name','avatar']); 
+    
+    if(!profile) 
+        return res.status(400).json({msg:'Profile not found'});
+    res.json(profile);
+    } catch (err) { 
+        console.error(err.message);
+        if(err.kind=='ObjectId'){
+            return res.status(400).json({msg:'Profile not found'});   
+        }
+        res.status(500).send('Server Error');
+
+    }
+}); 
+
 module.exports =router;
