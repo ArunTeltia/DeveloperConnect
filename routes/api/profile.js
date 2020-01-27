64582c902049465c8099bd289a2319faca00422e
ArunTeltia <teltia.arun18@gmail.com>
2020-01-27 20:00:42 +0530

    routes profile get /me

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router=express.Router();
 const auth = require('../../middleware/auth')
-
+const Profile = require('../../model/Profile');
+const user = require('../../model/User');
 //@route GET api/profile/me
 //@desc get current user profile
 //@acess private
-router.get('/',(req,res) => res.send('Profile route'));
-
+router.get('/me',auth , async (req,res) => {
+    try{
+        const profile = await (await Profile.findOne({user:req.user.id})).populated('user',['name','avatar']);
+        if(!profile){
+            return res.status(400).json({msg:'There is no profile for this user'});
+        }
+        res.json(profile);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+//now we have to make a route that create a file of me as there is no profile present for now
 
 module.exports =router;
