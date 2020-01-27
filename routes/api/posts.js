14a8c93d8de6f87b27b7a4e41f029679f93e678c
ArunTeltia <teltia.arun18@gmail.com>
2020-01-28 04:10:32 +0530

    delete post+verify user

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -52,7 +52,7 @@ router.post(
 
 router.get("/", auth, async (req, res) => {
   try {
-    const posts = await Post.find().sort({date:-1});
+    const posts = await Post.find().sort({ date: -1 });
     res.json(posts);
   } catch (err) {
     console.error(err.message);
@@ -64,21 +64,52 @@ router.get("/", auth, async (req, res) => {
 //@desc  get post by ID
 //@acess Private
 router.get("/:id", auth, async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
- 
-      if(!post){
-          return res.statusMessage(404).json({msg:'Post not found'});
-      }
-      res.json(posts) ; 
-    } catch (err) {
+  try {
+    const post = await Post.findById(req.params.id);
 
-      console.error(err.message);
-      if(err.kind==='ObjectId'){
-          //the id that we are passing dont exist
-        return res.statusMessage(404).json({msg:'Post not found'});
+    if (!post) {
+      return res.statusMessage(404).json({ msg: "Post not found" });
     }
-      res.status(500).send("Server Error");
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      //the id that we are passing dont exist
+      return res.statusMessage(404).json({ msg: "Post not found" });
     }
-  });
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route    DELETE api/posts/:id
+// @desc     Delete a post
+// @access   Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check for ObjectId format and post
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    // Check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.error(err.message);
+
+    res.status(500).send("Server Error");
+  }
+});
+
+
+
+
+
 module.exports = router;
