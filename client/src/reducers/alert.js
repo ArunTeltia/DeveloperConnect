6b07f3921232c3d187efa5c24bcbf9d8d0857171
ArunTeltia <teltia.arun18@gmail.com>
2020-01-29 04:52:35 +0530

    REDUX TOOLS SETUP

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,12 +1,9 @@
 import {SET_ALERT,REMOVE_ALERT} from '../actions/types';
-const initialState = [
-    {
-        id:1,
-        msg:'Please '
-    }
-];
+const initialState = [];
 
 export default function(state =initialState,action){
+
+    const{type,payload} =action;
     switch(type){
         case 'SET_ALERT':
             case SET_ALERT:
