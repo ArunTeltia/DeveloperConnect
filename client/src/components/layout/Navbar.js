271af12db1a68335e68c5e60d92040a272357c05
ArunTeltia <teltia.arun18@gmail.com>
2020-01-29 01:28:18 +0530

    Landing+NavBar

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+ 
+export const Navbar = () => {
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <a href="index.html">
+          <i class="fas fa-code"></i> DevConnector
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <a href="profiles.html">Developers</a>
+        </li>
+        <li>
+          <a href="register.html">Register</a>
+        </li>
+        <li>
+          <a href="login.html">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
